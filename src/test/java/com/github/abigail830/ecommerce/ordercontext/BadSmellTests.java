e4c289b7ef351940e8ffd096b17b3b6a0b7af3f3
SaraQian <abigail830@163.com>
2020-10-02 09:27:50 +0800

    adding test bad smell for archguard testing

---
+++ src/test/java/com/github/abigail830/ecommerce/ordercontext/BadSmellTests.java
@@ -0,0 +1,16 @@
+package com.github.abigail830.ecommerce.ordercontext;
+
+import org.junit.jupiter.api.Test;
+
+class BadSmellTests {
+
+    @Test
+    void should_sleep() throws InterruptedException {
+        Thread.sleep(100);
+    }
+
+    @Test
+    void should_print_without_assert() throws InterruptedException {
+        System.out.println("Only print");
+    }
+}
